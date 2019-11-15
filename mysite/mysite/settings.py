cbe3763b788820da181b927fb0d0a28411b2120a
Shiv <shivfsdev@gmail.com>
2019-11-15 05:52:59 +0000

    - docker command to launch tests and flake works      - docker-compose run mysite sh -c "python manage.py test && flake8"  - Need to run docker build after any code changes to generate new image [#169115692]

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -14,6 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+DB_DIR = '/db'
 
 
 # Quick-start development settings - unsuitable for production
@@ -76,7 +77,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(DB_DIR, 'db.sqlite3'),
     }
 }
 
@@ -86,16 +87,20 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME': 'django.contrib.auth.password_validation.\
+                 UserAttributeSimilarityValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'NAME': 'django.contrib.auth.password_validation.\
+                 MinimumLengthValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation.\
+                 CommonPasswordValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation.\
+                 NumericPasswordValidator',
     },
 ]
 
--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
-from django.views.decorators.http import require_http_methods
 import datetime
+
 
 # @require_http_methods(["GET"])
 def current_datetime(request):
@@ -8,17 +8,18 @@ def current_datetime(request):
     html = "<html><body>It is now %s.</body></html>" % now
     return HttpResponse(html)
 
+
 def year_archive(request, year):
-    now = datetime.datetime.now()
     html = "<html><body>It is now year %d.</body></html>" % year
     return HttpResponse(html)
 
+
 def month_archive(request, year, month):
-    now = datetime.datetime.now()
-    html = "<html><body>It is now month %d, year %d.</body></html>" % (month, year)
+    html = "<html><body>It is now month %d, year %d.</body></html>" \
+           % (month, year)
     return HttpResponse(html)
 
+
 def article_detail(request):
-    now = datetime.datetime.now()
     html = "<html><body>It is now article_detail.</body></html>"
     return HttpResponse(html)
