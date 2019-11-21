da5cfedddaabace56503241530f0ddc4a12ae79f
Shiv <shivfsdev@gmail.com>
2019-11-21 08:55:55 +0000

    - added support for creating super user [#169115692]

--- mysite/core/tests/test_models.py
+++ mysite/core/tests/test_models.py
@@ -32,3 +32,13 @@ class ModelTests(TestCase):
         with self.assertRaises(ValueError):
             get_user_model().objects.create_user( None, 'test123')
 
+    def test_create_new_superuser(self):
+        """test creaiting a new superuser"""
+        user = get_user_model().objects.create_superuser(
+                'test@londonappdev.com',
+                'test123'
+        )
+        
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
+
