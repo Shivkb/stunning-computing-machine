c29f3089265f30db3f6da217b5f72dd62d852eeb
Shiv <shivfsdev@gmail.com>
2019-11-22 07:30:40 +0000

    - The management command 'wait_for_db' added (with tests)  - Working for bare metal [#169115692]

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -80,8 +80,11 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(DB_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': os.environ.get('DB_HOST'),
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
     }
 }
 
