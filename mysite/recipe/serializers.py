49e2df85e143504803df1c47b5cf608a76f9414d
Shiv <shivfsdev@gmail.com>
2019-12-05 07:56:07 +0000

    Added custom recipe detail view [#169115692]

--- mysite/recipe/serializers.py
+++ mysite/recipe/serializers.py
@@ -37,3 +37,16 @@ class RecipeSerializer(serializers.ModelSerializer):
         fields = ('id', 'title', 'ingredients', 'tags', 'time_minutes',
                   'price', 'link')
         read_only_fields = ('id',)
+
+
+class RecipeDetailSerializer(RecipeSerializer):
+    """Serializer for the detail Recipe object"""
+    ingredients = IngredientSerializer(many=True, read_only=True)
+
+    tags = TagSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Recipe
+        fields = ('id', 'title', 'ingredients', 'tags', 'time_minutes',
+                  'price', 'link')
+        read_only_fields = ('id',)
